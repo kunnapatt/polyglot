5c0ce8bb10f3348412ee682050f5820bb7c3bf22
kunnapatt <folk_qq@hotmail.com>
2019-05-30 19:45:12 +0700

    try_building_jl801

--- src/polyglot/ext/jl5/ast/JL5Ext.java
+++ src/polyglot/ext/jl5/ast/JL5Ext.java
@@ -14,12 +14,12 @@ ram and the accompanying materials are made available under
  * This program and the accompanying materials are made available under
  * the terms of the Lesser GNU Public License v2.0 which accompanies this
  * distribution.
- * 
+ *
  * The development of the Polyglot project has been supported by a
  * number of funding sources, including DARPA Contract F30602-99-1-0533,
  * monitored by USAF Rome Laboratory, ONR Grants N00014-01-1-0968 and
  * N00014-09-1-0652, NSF Grants CNS-0208642, CNS-0430161, CCF-0133302,
- * and CCF-1054172, AFRL Contract FA8650-10-C-7022, an Alfred P. Sloan 
+ * and CCF-1054172, AFRL Contract FA8650-10-C-7022, an Alfred P. Sloan
  * Research Fellowship, and an Intel Research Ph.D. Fellowship.
  *
  * See README for contributors.
@@ -39,15 +39,14 @@ public class JL5Ext extends Ext_c {
     private static final long serialVersionUID = SerialVersionUID.generate();
 
     public static JL5Ext ext(Node n) {
-        Ext e = n.ext();
-        while (e != null && !(e instanceof JL5Ext)) {
-            e = e.ext();
-        }
-        if (e == null) {
+        Ext ans = null;
+        for (Ext e = n.ext(); e != null; e = e.ext())
+            if (e instanceof JL5Ext) ans = e;
+        if (ans == null) {
             throw new InternalCompilerError("No JL5 extension object for node "
                     + n + " (" + n.getClass() + ")", n.position());
         }
-        return (JL5Ext) e;
+        return (JL5Ext) ans;
     }
 
     @Override
